c7d7e2c7b35f2b62c9bf01adc7e1e29b7a2a3fc2
Jason Kapp <mtbkapp@gmail.com>
2020-12-01 07:57:10 -0700

    Day 1

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject advent-of-code-2020 "0.1.0-SNAPSHOT"
+  :description "Solutions to Advent of Code 2020 puzzles"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
---
+++ src/advent_of_code_2020/day1.clj
@@ -0,0 +1,41 @@
+(ns advent-of-code-2020.day1
+  (:require [clojure.java.io :as io]))
+
+
+(defn read-entries
+  []
+  (with-open [rdr (io/reader (io/resource "day1.txt"))]
+    (into []
+          (map #(Long/valueOf %))
+          (line-seq rdr))))
+
+
+#_(distinct? (read-entries))
+
+
+#_(prn (solve-part-1))
+(defn solve-part-1
+  []
+  (let [es (read-entries)
+        [x y] (first (for [x entries
+                           y entries
+                           :when (and (not= x y)
+                                      (= 2020 (+ x y)))]
+                       [x y]))]
+    [x y (* x y)]))
+
+
+#_(prn (solve-part-2))
+(defn solve-part-2
+  []
+  (let [es (read-entries)
+        [x y z] (first (for [x entries
+                             y entries
+                             z entries
+                             :when (and (not= x y) 
+                                        (not= y z)
+                                        (not= x z)
+                                        (= 2020 (+ x y z)))]
+                         [x y z]))]
+    [x y z (* x y z)]))
+
