ca361628bf70933d18216ce30544507bc18ec8aa
Jason Kapp <mtbkapp@gmail.com>
2020-12-08 09:38:37 -0700

    part2

--- src/advent_of_code_2020/day8.clj
+++ src/advent_of_code_2020/day8.clj
@@ -1,7 +1,5 @@
 (ns advent-of-code-2020.day8
-  (:require [advent-of-code-2020.utils :as utils]
-            [clojure.string :as string]
-            [instaparse.core :as insta]))
+  (:require [advent-of-code-2020.utils :as utils]))
 
 
 (def pattern #"(acc|jmp|nop) (\+|\-)([0-9]+)")
@@ -10,43 +8,49 @@
 (defn decode-instr
   [instr]
   (let [[_ op dir mag :as x] (re-matches pattern instr)]
-    [(keyword op) ((if (= "-" dir) - +) (Long/valueOf mag))]
-    
-    ))
+    [(keyword op) ((if (= "-" dir) - identity) (Long/valueOf mag))]))
 
-
-(defn init 
-  [prg]
-  {:pc 0 :a 0})
-
-
-(defmulti next-state*
-  (fn [state [op :as instr]]
-    op))
-
-(defmethod next-state* :nop
-  [state instr]
-  (update state :pc inc))
-
-(defmethod next-state* :acc
-  [state [_ arg]]
-  (-> state
-      (update :a + arg)
-      (update :pc inc)))
-
-(defmethod next-state* :jmp
-  [state [_ arg]]
-  (update state :pc + arg))
 
 (defn read-input
   []
   (utils/read-lines (map decode-instr) "day8.txt"))
 
-#_(find-loop (read-input))
-(defn find-loop
+
+(defn run-prg
   [prg]
-  (loop [{:keys [pc] :as state} (init prg) executed #{}]
-    (if (contains? executed pc)
-      state
-      (recur (next-state* state (get prg pc))
-             (conj executed pc)))))
+  (loop [pc 0 a 0 executed #{}]
+    (cond (contains? executed pc) [:loop a pc]
+          (= (count prg) pc) [:terminate a pc]
+          (not (<= 0 pc (dec (count prg)))) [:out-of-bounds a pc]
+          :else (let [[op arg] (get prg pc)
+                      ne (conj executed pc)]
+                  (cond (= :nop op) (recur (inc pc) a ne)
+                        (= :acc op) (recur (inc pc) (+ a arg) ne)
+                        (= :jmp op) (recur (+ pc arg) a ne))))))
+
+
+#_(prn (solve-part1))
+(defn solve-part1
+  []
+  (run-prg (read-input)))
+
+
+(defn flip-op
+  [prg last-change]
+  (let [i (inc last-change)
+        [op] (get prg i)]
+    (if (= :acc op)
+      (recur prg i)
+      [i (update-in prg [i 0] {:jmp :nop :nop :jmp})])))
+
+
+#_(prn (solve-part2))
+(defn solve-part2
+  []
+  (let [prg (read-input)]
+    (loop [last-change -1]
+      (let [[change test-prg] (flip-op prg last-change)
+            [result a pc] (run-prg test-prg)]
+        (if (= result :terminate)
+          [change a]
+          (recur change))))))
