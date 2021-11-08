aca57e8341f7820398e170ee9cd12b559354fa87
Jason Kapp <mtbkapp@gmail.com>
2021-11-08 14:35:19 -0700

    solution to part2, read the whole problem

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [instaparse "1.4.10"]
-                 [org.clojure/tools.logging "1.1.0"]])
+                 [instaparse "1.4.10"]])
--- src/advent_of_code_2020/day22.clj
+++ src/advent_of_code_2020/day22.clj
@@ -1,7 +1,6 @@
 (ns advent-of-code-2020.day22
   (:require [clojure.java.io :as io]
-            [clojure.string :as string]
-            [clojure.tools.logging :as log]))
+            [clojure.string :as string]))
 
 
 (def test-input
@@ -21,11 +20,13 @@ Player 2:
 
 (def real-input (slurp (io/resource "day22.txt")))
 
+(def empty-queue clojure.lang.PersistentQueue/EMPTY)
+
 #_(read-input test-input)
 (defn read-input-deck
   [deck-input]
   (let [[id-line & card-lines] (string/split-lines deck-input)]
-     (into clojure.lang.PersistentQueue/EMPTY
+     (into empty-queue 
            (map #(Long/valueOf %))
            card-lines)))
 
@@ -60,45 +61,16 @@ Player 2:
              +
              (reverse deck)))
 
+
+#_(solve-part1 (read-input test-input))
+#_(solve-part1 (read-input real-input))
 (defn solve-part1 
   [[deck0 deck1 :as state]]
   (if (or (empty? deck0) (empty? deck1))
     (apply max (map score-deck state))
     (recur (play-round state))))
 
-#_(solve-part1 (read-input test-input))
-#_(solve-part1 (read-input real-input))
 
-(defn play-round
-  [configs p1-deck p2-deck]
-  (if (and false (contains? configs [p1-deck p2-deck]))
-    {:winner :p1}
-    (if (< (peek p1-deck) (peek p2-deck))
-      {:winner :p2}
-      {:winnder :p1})))
-
-(defn recur?
-  [p1-deck p2-deck]
-  (and (<= (peek p1-deck) (count (pop p1-deck)))
-       (<= (peek p2-deck) (count (pop p2-deck)))))
-
-
-#_(as-> (recursive-combat
-            (into clojure.lang.PersistentQueue/EMPTY [9 2 6 3 1])
-            (into clojure.lang.PersistentQueue/EMPTY [5 8 4 7 10])) $
-    (do
-      (println "Score: " (calc-score $))
-      $)
-    (update $ :p1-deck seq)
-    (update $ :p2-deck seq))
-
-#_(-> (apply recursive-combat (read-input real-input))
-      calc-score
-      )
-
-; doesn't finish?
-; bug? need to check configs in another place for duplicate?
-; ok now it finishes but it's incorrect
 (defn recursive-combat
   [p1-deck p2-deck]
   (loop [configs #{}
@@ -120,32 +92,29 @@ Player 2:
             p1-deck2 (pop p1-deck)
             p2-card (peek p2-deck)
             p2-deck2 (pop p2-deck)
-            winner (if (contains? configs [p1-deck p2-deck])
-                     :p1
-                     (if (and (<= p1-card (count p1-deck2))
-                              (<= p2-card (count p2-deck2)))
-                       ; winner by recursion
-                       (:winner (recursive-combat p1-deck2 p2-deck2))
-                       ; winner by card value
-                       (if (< p2-card p1-card) :p1 :p2)))
+            winner (if (and (<= p1-card (count p1-deck2))
+                            (<= p2-card (count p2-deck2)))
+                     ; winner by recursion
+                     (:winner (recursive-combat 
+                                (into empty-queue (take p1-card) p1-deck2)
+                                (into empty-queue (take p2-card) p2-deck2)))
+                     ; winner by card value
+                     (if (< p2-card p1-card) :p1 :p2))
             config2 (conj configs [p1-deck p2-deck])]
         (if (= :p1 winner)
           (recur config2 (into p1-deck2 [p1-card p2-card]) p2-deck2)
-          (recur config2 p1-deck2 (into p2-deck2 [p2-card p1-card]))))
-      #_(let [winner (if (recur? p1-deck p2-deck)
-                     (:winner (recursive-combat (pop p1-deck) (pop p2-deck)))
-                     (:winner (play-round configs p1-deck p2-deck)))
-            config2 (conj configs [p1-deck p2-deck])]
-        (if (= :p1 winner)
-          (recur config2
-                 (into (pop p1-deck) [(peek p1-deck) (peek p2-deck)])
-                 (pop p2-deck))
-          (recur config2
-                 (pop p1-deck)
-                 (into (pop p2-deck) [(peek p2-deck) (peek p1-deck)])))))))
+          (recur config2 p1-deck2 (into p2-deck2 [p2-card p1-card])))))))
+
 
 (defn calc-score
   [{:keys [winner p1-deck p2-deck]}]
-  (if (= :p1 winner)
-    (score-deck p1-deck)
-    (score-deck p2-deck)))
+  (score-deck (if (= :p1 winner) p1-deck p2-deck)))
+
+
+#_(prn (solve-part2 test-input))
+#_(prn (solve-part2 real-input))
+(defn solve-part2
+  [input]
+  (let [[p1-deck p2-deck] (read-input input)]
+    (calc-score (recursive-combat p1-deck p2-deck))))
+
